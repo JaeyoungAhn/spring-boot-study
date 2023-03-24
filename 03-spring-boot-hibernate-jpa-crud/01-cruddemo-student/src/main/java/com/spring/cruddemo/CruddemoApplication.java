417bce377a51f21e30a8a20d9ceff1107a5b667d
JaeyoungAhn <quaki@naver.com>
2023-03-25 00:51:05 +0900

    structure update

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/spring/cruddemo/CruddemoApplication.java
@@ -23,7 +23,7 @@ public class CruddemoApplication {
 
 			createMultipleStudents(studentDAO);
 
-//			readStudent(studentDAO);R
+//			readStudent(studentDAO);
 
 //			queryForStudents(studentDAO);
 
